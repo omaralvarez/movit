94c2e8d4c7fafa2c359cbdbb1e0ba7cecc84dd7a
omaralvarez <omar.alvarez@udc.es>
2016-12-11 21:02:26 +0100

    Added admin action to update episode counts

--- frontend/admin.py
+++ frontend/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 from .models import Episode
 from .models import TVShow
 
-admin.site.register(Episode)
+def update_ep_counts(modeladmin, request, queryset):
+    for obj in queryset:
+        ep_count = Episode.objects.filter(tv_show__name=obj.name).count
+        obj.episode_count = ep_count
+        obj.save()
+
+
+update_ep_counts.short_description = "Update episode counts"
+
+class EpisodeAdmin(admin.ModelAdmin):
+    actions = [update_ep_counts]
+
+admin.site.register(Episode, EpisodeAdmin)
 admin.site.register(TVShow)
